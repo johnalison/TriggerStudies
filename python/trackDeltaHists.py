14e8b690d52822edf57c1ec7dd7111af539238f1
Ian Harris <ianh@andrew.cmu.edu>
2018-11-15 16:49:18 -0500

    Fixed whitespace

--- python/trackDeltaHists.py
+++ python/trackDeltaHists.py
@@ -7,11 +7,11 @@ class TrackDeltaHists:
 
     def __init__(self,name,outFile):
         self.name = name
-	self.thisDir = outFile.mkdir(self.name)
-	self.dSip3dSig    = makeHist(self.thisDir, "dSip3dSig",   "#Delta Sip3dSig;#Delta Sip3dSig;Entries",       100,-10,10)
+        self.thisDir = outFile.mkdir(self.name)
+        self.dSip3dSig    = makeHist(self.thisDir, "dSip3dSig",   "#Delta Sip3dSig;#Delta Sip3dSig;Entries",       100,-10,10)
         self.dSip3dVal    = makeHist(self.thisDir, "dSip3dVal",   "#Delta Sip3dVal;#Delta Sip3dVal;Entries",       100,-0.4,0.4)
-	self.zoomip3dVal  = makeHist(self.thisDir, "zoomip3dVal", "#Delta Sip3dVal;#Delta Sip3dVal;Entries",       100,-0.015,0.015)
-	self.ip3dErr      = makeHist(self.thisDir, "ip3dErr",     "ip3d Val/Sig;ip3d Val/Sig;Entries",             100,-0.05,0.05)
+        self.zoomip3dVal  = makeHist(self.thisDir, "zoomip3dVal", "#Delta Sip3dVal;#Delta Sip3dVal;Entries",       100,-0.015,0.015)
+        self.ip3dErr      = makeHist(self.thisDir, "ip3dErr",     "ip3d Val/Sig;ip3d Val/Sig;Entries",             100,-0.05,0.05)
         self.dSip2dSig    = makeHist(self.thisDir, "dSip2dSig",   "#Delta Sip2dSig;#Delta Sip2dSig;Entries",       100,-10,10)
         self.dSip2dVal    = makeHist(self.thisDir, "dSip2dVal",   "#Delta Sip2dVal;#Delta Sip2dVal;Entries",       100,-0.4,0.4)
         self.zoomip2dVal  = makeHist(self.thisDir, "zoomip2dVal", "#Delta Sip2dVal;#Delta Sip2dVal;Entries",       100,-0.015,0.015)
@@ -27,16 +27,16 @@ class TrackDeltaHists:
         self.dPParRatio   = makeHist(self.thisDir, "dPParRatio",  "#Delta PParRatio;#Delta PParRatio;Entries",     100,-0.05,0.05)
 
     def Fill(self,offTrack,matchedTrack):
-	d3Sig = offTrack.ip3dSig - matchedTrack.ip3dSig
-	d3Val = offTrack.ip3dVal - matchedTrack.ip3dVal
+        d3Sig = offTrack.ip3dSig - matchedTrack.ip3dSig
+        d3Val = offTrack.ip3dVal - matchedTrack.ip3dVal
         d2Sig = offTrack.ip2dSig - matchedTrack.ip2dSig
         d2Val = offTrack.ip2dVal - matchedTrack.ip2dVal
-	self.dSip3dSig    .Fill(d3Sig)
+        self.dSip3dSig    .Fill(d3Sig)
         self.dSip3dVal    .Fill(d3Val)
-	self.ip3dErr      .Fill(d3Val/d3Sig)
-	self.ip2dErr      .Fill(d2Val/d2Sig)
-	self.zoomip3dVal  .Fill(d3Val)
-	self.zoomip2dVal  .Fill(d2Val)
+        self.ip3dErr      .Fill(d3Val/d3Sig)
+        self.ip2dErr      .Fill(d2Val/d2Sig)
+        self.zoomip3dVal  .Fill(d3Val)
+        self.zoomip2dVal  .Fill(d2Val)
         self.dSip2dSig    .Fill(d2Sig)
         self.dSip2dVal    .Fill(d2Val)
         self.dDecayLenVal .Fill(offTrack.decayLenVal - matchedTrack.decayLenVal)
@@ -50,15 +50,15 @@ class TrackDeltaHists:
         self.dPParRatio   .Fill(offTrack.pParRatio - matchedTrack.pParRatio)
 
     def Write(self,outFile):
-	self.thisDir.cd()
-	self.dSip3dSig    .Write()
+        self.thisDir.cd()
+        self.dSip3dSig    .Write()
         self.dSip3dVal    .Write()
-	self.zoomip3dVal  .Write()
-	self.ip3dErr      .Write()
+        self.zoomip3dVal  .Write()
+        self.ip3dErr      .Write()
         self.dSip2dSig    .Write()
         self.dSip2dVal    .Write()
-	self.zoomip2dVal  .Write()
-	self.ip2dErr      .Write()
+        self.zoomip2dVal  .Write()
+        self.ip2dErr      .Write()
         self.dDecayLenVal .Write()
         self.dJetDistVal  .Write()
         self.dPtRel       .Write()
@@ -68,5 +68,5 @@ class TrackDeltaHists:
         self.dDeltaR      .Write()
         self.dPtRatio     .Write()
         self.dPParRatio   .Write()
-	outFile           .cd()
+        outFile           .cd()
 
