37b7f24b61f2d2379a6fd67abdb51fb3db93ae02
John Alison <johnalison@cmu.edu>
2019-02-21 18:24:56 +0100

    Adding other files from luca

--- NtupleAna/run/runAll.sh
+++ NtupleAna/run/runAll.sh
@@ -1,4 +1,4 @@
-RunPeriod=runD
+RunPeriod=$1
 ./TriggerStudies/NtupleAna/run/runTtbar_runN.sh $RunPeriod &
 ./TriggerStudies/NtupleAna/run/runST_runN.sh $RunPeriod &
 ./TriggerStudies/NtupleAna/run/runSantiT_runN.sh $RunPeriod &
