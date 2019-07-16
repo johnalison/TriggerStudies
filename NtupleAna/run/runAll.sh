a69032a9c3faf39b6b070a1baa83536738adf2d6
John Alison <johnalison@cmu.edu>
2019-07-16 08:18:11 -0500

    Cleaning up the run dir

--- NtupleAna/run/runAll.sh
+++ NtupleAna/run/runAll.sh
@@ -1,9 +1,4 @@
-#RunPeriod=$1
-#./TriggerStudies/NtupleAna/run/runTtbar_runN.sh $RunPeriod &
-#./TriggerStudies/NtupleAna/run/runST_runN.sh $RunPeriod &
-#./TriggerStudies/NtupleAna/run/runSantiT_runN.sh $RunPeriod &
-
 RunPeriod=$1
-./TriggerStudies/NtupleAna/run/runTtbar2017_runN.sh $RunPeriod &
-./TriggerStudies/NtupleAna/run/runST2017_runN.sh $RunPeriod &
-./TriggerStudies/NtupleAna/run/runSantiT2017_runN.sh $RunPeriod &
+./TriggerStudies/NtupleAna/run/runTTbar2017_runN.sh $RunPeriod &
+./TriggerStudies/NtupleAna/run/runST2017_wT_runN.sh $RunPeriod &
+./TriggerStudies/NtupleAna/run/runST2017_wAntiT_runN.sh $RunPeriod &
