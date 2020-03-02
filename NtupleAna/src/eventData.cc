3d2eadfd251f9e39e53e86db00470cca03f098f6
Sebastian Wuchterl <sebastian.wuchterl@cern.ch>
2020-03-02 16:28:02 +0100

    change eta range in eventData

--- NtupleAna/src/eventData.cc
+++ NtupleAna/src/eventData.cc
@@ -24,7 +24,7 @@ eventData::eventData(TChain* _treeRAW, TChain* _treeAOD, bool mc, std::string y,
     //
     inputBranch(treeAOD, "Run",             runAOD);
     inputBranch(treeAOD, "Evt",           eventAOD);
-    
+
     for(long int eAOD = 0; eAOD < treeEventsAOD; eAOD++){
       treeAOD->GetEntry(eAOD);
       AODEvents.push_back(std::make_pair(runAOD, eventAOD));
@@ -80,37 +80,37 @@ eventData::eventData(TChain* _treeRAW, TChain* _treeAOD, bool mc, std::string y,
 
   }
 
-  treeAOD->SetBranchStatus("Run", 1);  
-  treeAOD->SetBranchStatus("Evt", 1);  
+  treeAOD->SetBranchStatus("Run", 1);
+  treeAOD->SetBranchStatus("Evt", 1);
   //treeAOD->BuildIndex("Run","Evt");
-  TTreeIndex *index = new TTreeIndex(treeAOD,"Run", "Evt"); 
+  TTreeIndex *index = new TTreeIndex(treeAOD,"Run", "Evt");
   treeAOD->SetTreeIndex(index);
-    
-    
-  treeRAW->SetBranchStatus("Run", 1);  
-  treeRAW->SetBranchStatus("Evt", 1);  
+
+
+  treeRAW->SetBranchStatus("Run", 1);
+  treeRAW->SetBranchStatus("Evt", 1);
   //treeRAW->BuildIndex("Run","Evt");
-  //TTreeIndex *indexRaw = new TTreeIndex(treeRAW,"Run", "Evt"); 
+  //TTreeIndex *indexRaw = new TTreeIndex(treeRAW,"Run", "Evt");
   //treeRAW->SetTreeIndex(indexRaw);
-    
-    
+
+
   treeRAW->AddFriend(treeAOD);
-    
-        
+
+
   //std::cout << "eventData::eventData() tree->Lookup(true)" << std::endl;
   ///tree->Lookup(true);
   //std::cout << "eventData::eventData() tree->LoadTree(0)" << std::endl;
   //treeRAW->LoadTree(0);
-      
+
   inputBranch(treeRAW, "Run",             run);
   //inputBranch(tree, "luminosityBlock", lumiBlock);
   inputBranch(treeRAW, "Evt",           event);
-        
-    
+
+
   inputBranch(treeAOD, "Run",             runAOD);
   //inputBranch(tree, "luminosityBlock", lumiBlock);
   inputBranch(treeAOD, "Evt",           eventAOD);
-    
+
   std::cout << "eventData::eventData() Initialize jets and muons" << std::endl;
   std::string jetSFName = year;
   if(jetSFName == "2018") jetSFName = "deepcsv2018";
@@ -118,7 +118,7 @@ eventData::eventData(TChain* _treeRAW, TChain* _treeAOD, bool mc, std::string y,
   offTreeJets  = new nTupleAnalysis::jetData( "Jet",  treeRAW, true, isMC,  jetDetailLevel, "",      jetSFName );
   pfTreeJets   = new nTupleAnalysis::jetData( "Jet",  treeRAW, true, false, jetDetailLevel, "PFJet."       );
   caloTreeJets = new nTupleAnalysis::jetData( "Jet",  treeRAW, true, false, jetDetailLevel, "CaloJet."     );
-    
+
   //treeMuons    = new nTupleAnalysis::muonData("PFMuon",     treeRAW);
   //treeElecs    = new nTupleAnalysis::elecData("PFElectron", treeRAW);
   treeMuons    = new nTupleAnalysis::muonData("PatMuon",     treeRAW, true, isMC, year);
@@ -127,7 +127,7 @@ eventData::eventData(TChain* _treeRAW, TChain* _treeAOD, bool mc, std::string y,
   treePVs    = new nTupleAnalysis::vertexData("PV",     treeRAW);
   offTreePVs = new nTupleAnalysis::vertexData("PV",     treeAOD);
 
-} 
+}
 
 void eventData::update(int e){
   if(debug){
@@ -142,14 +142,14 @@ void eventData::update(int e){
   }
   Long64_t loadStatus = treeRAW->LoadTree(e);
   if(loadStatus<0){
-    std::cout << "Error "<<loadStatus<<" getting event "<<e<<std::endl; 
+    std::cout << "Error "<<loadStatus<<" getting event "<<e<<std::endl;
     return;
   }
 
 
 //  Long64_t loadStatusAOD = treeAOD->LoadTree(e);
 //  if(loadStatusAOD<0){
-//    std::cout << "Error "<<loadStatus<<" getting AOD event "<<e<<std::endl; 
+//    std::cout << "Error "<<loadStatus<<" getting AOD event "<<e<<std::endl;
 //    return;
 //  }
 
@@ -158,14 +158,14 @@ void eventData::update(int e){
   if(debug) std::cout<<"Got Entry "<<e<<std::endl;
 
   if((run != runAOD) || (event != eventAOD)){
-    if(debug) std::cout << "Run: " << run << " vs " << runAOD  << "  Evt: " << event << " vs " << eventAOD << std::endl;  
+    if(debug) std::cout << "Run: " << run << " vs " << runAOD  << "  Evt: " << event << " vs " << eventAOD << std::endl;
     //std::cout << "Tryuing with inedex " << treeAOD->GetEntryWithIndex(run,event)  << std::endl;
   }
 
   //if((run != runAOD) || (event != eventAOD)){
-  //  if(debug) std::cout << "Run: " << run << " vs " << runAOD  << "  Evt: " << event << " vs " << eventAOD << std::endl;  
-  //  std::cout << "Run: " << run << " vs " << runAOD  << "  Evt: " << event << " vs " << eventAOD << std::endl;  
-  //  
+  //  if(debug) std::cout << "Run: " << run << " vs " << runAOD  << "  Evt: " << event << " vs " << eventAOD << std::endl;
+  //  std::cout << "Run: " << run << " vs " << runAOD  << "  Evt: " << event << " vs " << eventAOD << std::endl;
+  //
   //  std::cout << "Try manual lookup" << std::endl;
   //  Int_t runToPrint = -99;
   //  treeAOD->GetEntries();
@@ -192,16 +192,16 @@ void eventData::update(int e){
   }
 
   if(event != eventAOD){
-    if(debug) std::cout << "evt: " << event << " vs " << eventAOD << std::endl;  
+    if(debug) std::cout << "evt: " << event << " vs " << eventAOD << std::endl;
     return;
   }
 
 
-  offJets  = offTreeJets->getJets(20,1e6,2.4);
-  pfJets   = pfTreeJets ->getJets(20,1e6,2.4);
-  caloJets = caloTreeJets ->getJets(30,1e6,2.4);
-  muons    = treeMuons  ->getMuons(20, 2.4);
-  elecs    = treeElecs  ->getElecs(30, 2.4);
+  offJets  = offTreeJets->getJets(20,1e6,4.5);
+  pfJets   = pfTreeJets ->getJets(20,1e6,4.5);
+  caloJets = caloTreeJets ->getJets(30,1e6,4.5);
+  muons    = treeMuons  ->getMuons(20, 4.5);
+  elecs    = treeElecs  ->getElecs(30, 4.5);
 
   pvs    = treePVs     ->getVerticies();
   offPVs = offTreePVs  ->getVerticies();
@@ -214,7 +214,7 @@ void eventData::update(int e){
 void eventData::dump(){
 
   std::cout << "   Run: " << run    << std::endl;
-  std::cout << " Event: " << event  << std::endl;  
+  std::cout << " Event: " << event  << std::endl;
   //std::cout << "Weight: " << weight << std::endl;
   //std::cout << " allJets: " << allJets .size() << " |  selJets: " << selJets .size() << " | tagJets: " << tagJets.size() << std::endl;
   //std::cout << "allMuons: " << allMuons.size() << " | isoMuons: " << isoMuons.size() << std::endl;
@@ -224,5 +224,4 @@ void eventData::dump(){
 
 eventData::~eventData(){
   std::cout << "eventData::destroyed" << std::endl;
-} 
-
+}
