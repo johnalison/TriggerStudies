237552ee3bd8837672994cd967c916feb5f6f58c
Sebastian Wuchterl <sebastian.wuchterl@cern.ch>
2020-06-04 20:47:34 +0200

    add more default scripts

---
+++ NtupleAna/run/runTestPhase2Puppi.sh
@@ -0,0 +1,95 @@
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV0.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV0_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV2.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV2_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6_skimmedTracks.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_skimmedTracks_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV0_TICL.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV0_TICL_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV2_TICL.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV2_TICL.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6_TICL.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_TICL_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU_puppi.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6_TICL_skimmedTracks.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_TICL_skimmedTracks_puppi.root \
+    --isMC \
+    --doTracks \
+    --doPuppiJets \
+    --nevents -1
