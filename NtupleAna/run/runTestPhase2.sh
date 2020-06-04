7e042aefa44ef289ef8ee64768da52323561cee5
Sebastian Wuchterl <sebastian.wuchterl@cern.ch>
2020-06-04 20:47:05 +0200

    add ROC comparison plotting scripts and new default for running the analyzer

--- NtupleAna/run/runTestPhase2.sh
+++ NtupleAna/run/runTestPhase2.sh
@@ -1,68 +1,87 @@
-# BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
-#     --inputAOD INPUTS/newGT_v1/Offline/RelValCMSSW11/ttbar.root \
-#     --inputRAW INPUTS/newGT_v1/Online/RelValCMSSW11/ttbar_TrackV2.root  \
-#     -o $PWD \
-#     -y phase2 \
-#     --histogramming 1 \
-#     --histFile OUTPUT/newGT_v1/RelValCMSSW11/ttbar_TrackV2.root \
-#     --isMC \
-#     --doTracks \
-#     --nevents -1
-
-# BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
-#     --inputAOD INPUTS/newGT_v1/Offline/RelValCMSSW11_0_0/ttbar_NoPU.root \
-#     --inputRAW INPUTS/newGT_v1/Online/RelValCMSSW11_0_0/ttbar_NoPU.root  \
-#     -o $PWD \
-#     -y phase2 \
-#     --histogramming 1 \
-#     --histFile OUTPUT/newGT_v1/RelValCMSSW11_0_0/ttbar_NoPU.root \
-#     --isMC \
-#     --doTracks \
-#     --nevents -1
-
-# BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
-#     --inputAOD INPUTS/newGT_v1/Offline/CMSSW11_NewGT_v1/ttbar.root \
-#     --inputRAW INPUTS/newGT_v1/Online/CMSSW11_NewGT_v1/ttbar_TrackV2_crab.root  \
-#     -o $PWD \
-#     -y phase2 \
-#     --histogramming 1 \
-#     --histFile OUTPUT/newGT_v1/CMSSW11_NewGT_v1/ttbar_TrackV2_crab.root \
-#     --isMC \
-#     --doTracks \
-#     --nevents -1
-#
-# BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
-#     --inputAOD INPUTS/newGT_v1/Offline/CMSSW11_NewGT_v1/ttbar_NoPU.root \
-#     --inputRAW INPUTS/newGT_v1/Online/CMSSW11_NewGT_v1/ttbar_TrackV2_NoPU_crab.root  \
-#     -o $PWD \
-#     -y phase2 \
-#     --histogramming 1 \
-#     --histFile OUTPUT/newGT_v1/CMSSW11_NewGT_v1/ttbar_TrackV2_NoPU_crab.root \
-#     --isMC \
-#     --doTracks \
-#     --nevents -1
-#
-# BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
-#     --inputAOD INPUTS/newGT_v1/Offline/CMSSW11_NewGT_v1/ttbar.root \
-#     --inputRAW INPUTS/newGT_v1/Online/CMSSW11_NewGT_v1/ttbar_TrackV0_crab.root  \
-#     -o $PWD \
-#     -y phase2 \
-#     --histogramming 1 \
-#     --histFile OUTPUT/newGT_v1/CMSSW11_NewGT_v1/ttbar_TrackV0_crab.root \
-#     --isMC \
-#     --doTracks \
-#     --nevents -1
-#    --inputAOD root://cmsxrootd.fnal.gov//store/user/jda102//BTagNTuples/2017/crab_projects_v2/MuonEG/Run2017B-31Mar2018-v1/190712_205650/inputFiles_2017B_MINIAOD_All.root \
-
-
-
 BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
-    --inputAOD INPUTS/30_01_20_FullRelValStudy/Offline/ttbar_PU200_correctGT.root \
-    --inputRAW INPUTS/30_01_20_FullRelValStudy/Online/ttbar_PU200_TrackingV0_skimTracks_correctGT.root  \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV0.root  \
     -o $PWD \
     -y phase2 \
     --histogramming 1 \
-    --histFile OUTPUT/30_01_20_FullRelValStudy/ttbar_PU200_TrackingV0_skimTracks_correctGT.root \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV0.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV2.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV2.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6_skimmedTracks.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_skimmedTracks.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV0_TICL.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV0_TICL.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV2_TICL.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV2_TICL.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6_TICL.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_TICL.root \
+    --isMC \
+    --doTracks \
+    --nevents -1
+
+BTagAnalyzer ../scripts/BTagAnalyzer_cfg.py \
+    --inputAOD INPUT/25_05_20_HLTTDR/Offline/ttbar_NoPU.root \
+    --inputRAW INPUT/25_05_20_HLTTDR/Online/ttbar_NoPU_TrackingV6_TICL_skimmedTracks.root  \
+    -o $PWD \
+    -y phase2 \
+    --histogramming 1 \
+    --histFile OUTPUT/25_05_20_HLTTDR/ttbar_NoPU_TrackingV6_TICL_skimmedTracks.root \
     --isMC \
     --doTracks \
     --nevents -1
